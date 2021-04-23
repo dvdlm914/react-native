89002277124bed3c185b09df1e501d51e729249a
dvdlm914 <dvdlm914@gmail.com>
2021-04-23 11:06:43 -0400

    animations added

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -8,6 +8,7 @@ import {
   Button,
   StyleSheet,
 } from "react-native";
+import * as Animatable from "react-native-animatable";
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
@@ -29,34 +30,36 @@ function RenderCampsite(props) {
   const { campsite } = props;
   if (campsite) {
     return (
-      <Card
-        featuredTitle={campsite.name}
-        image={{ uri: baseUrl + campsite.image }}
-      >
-        <Text style={{ margin: 10 }}>{campsite.description}</Text>
-        <View style={{ flexDirection: "row" }}>
-          <Icon
-            name={props.favorite ? "heart" : "heart-o"}
-            type="font-awesome"
-            color="#f50"
-            raised
-            reverse
-            onPress={() =>
-              props.favorite
-                ? console.log("already a fave")
-                : props.markFavorite()
-            }
-          />
-          <Icon
-            name="pencil"
-            type="font-awesome"
-            color="#5637DD"
-            raised
-            reverse
-            onPress={() => props.onShowModal()}
-          />
-        </View>
-      </Card>
+      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+        <Card
+          featuredTitle={campsite.name}
+          image={{ uri: baseUrl + campsite.image }}
+        >
+          <Text style={{ margin: 10 }}>{campsite.description}</Text>
+          <View style={{ flexDirection: "row" }}>
+            <Icon
+              name={props.favorite ? "heart" : "heart-o"}
+              type="font-awesome"
+              color="#f50"
+              raised
+              reverse
+              onPress={() =>
+                props.favorite
+                  ? console.log("already a fave")
+                  : props.markFavorite()
+              }
+            />
+            <Icon
+              name="pencil"
+              type="font-awesome"
+              color="#5637DD"
+              raised
+              reverse
+              onPress={() => props.onShowModal()}
+            />
+          </View>
+        </Card>
+      </Animatable.View>
     );
   }
   return <View />;
@@ -80,13 +83,15 @@ function RenderComments({ comments }) {
     );
   };
   return (
-    <Card title="comments">
-      <FlatList
-        data={comments}
-        renderItem={renderCommentItem}
-        keyExtractor={(item) => item.id.toString()}
-      ></FlatList>
-    </Card>
+    <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+      <Card title="comments">
+        <FlatList
+          data={comments}
+          renderItem={renderCommentItem}
+          keyExtractor={(item) => item.id.toString()}
+        ></FlatList>
+      </Card>
+    </Animatable.View>
   );
 }
 class CampsiteInfo extends Component {
